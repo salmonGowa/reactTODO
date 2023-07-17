4ad1cca0348e464a9d366b5f8be89ecdc1e7ac32
Salmon Gowa <67318860+salmonGowa@users.noreply.github.com>
2023-07-17 10:51:19 +0300

    <>

--- night-shade/src/components/Navbar.js
+++ night-shade/src/components/Navbar.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 function Navbar(props) {
   return (
-    <nav className="navbar navbar-light bg-light">
+    <nav className="navbar navbar-dark bg-dark">
   <div className="container-fluid">
-    <a className="navbar-brand">Navbar</a>
+    <a className="navbar-brand text-white">{props.name}</a>
     <form className="d-flex">
       <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"></input>
       <button className="btn btn-outline-success" type="submit">Search</button>
