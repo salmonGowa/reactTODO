34c241eded013c42d63c821aa8af41fc6a4ecbc6
Salmon Gowa <67318860+salmonGowa@users.noreply.github.com>
2023-07-18 17:17:27 +0300

    --

--- night-shade/src/components/section.js
+++ night-shade/src/components/section.js
@@ -6,10 +6,15 @@ function Section(props) {
     <h6 className="display-4">{props.title}</h6>
 
   <div className="row mt-5">
-    <div className="col-md-2">1</div>
-    <div className="col-md-4">2</div>
-    <div className="col-md-4">3</div>
-    <div className="col-md-2">4</div>
+    <div className="col-md-2"></div>
+    <div className="col-md-4">
+      <textarea className='form-control' cols="30" rows="10"></textarea>
+     
+    </div>
+    <div className="col-md-4">
+      <h4 className='text-success'>output</h4>
+    </div>
+    <div className="col-md-2"></div>
 
   </div>
   </>
